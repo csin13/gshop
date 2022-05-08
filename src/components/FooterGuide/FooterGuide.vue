d213595a342722169ddd4e1fc70a26c6719f035a
陈斯婧 <839294976@qq.com>
2022-05-08 16:15:41 +0800

    footer组件

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>FooterGuide</div>
+  <footer class="footer_guide">
+    <a
+      href="javascript:;"
+      class="guide_item"
+      :class="{ on: '/msite' === $route.path }"
+      @click="goTo('/msite')"
+    >
+      <span><i class="iconfont icon-waimai"></i><span> </span>外卖</span>
+    </a>
+    <a
+      href="javascript:;"
+      class="guide_item"
+      :class="{ on: '/search' === $route.path }"
+      @click="goTo('/search')"
+    >
+      <span> <i class="iconfont icon-sousuo"></i><span></span>搜索</span></a
+    ><a
+      href="javascript:;"
+      class="guide_item"
+      :class="{ on: '/order' === $route.path }"
+      @click="goTo('/order')"
+      ><span><i class="iconfont icon-dingdan"></i><span></span>订单</span></a
+    ><a
+      href="javascript:;"
+      class="guide_item"
+      :class="{ on: '/profile' === $route.path }"
+      @click="goTo('/profile')"
+      ><span><i class="iconfont icon-wode"></i><span></span>我的</span></a
+    >
+  </footer>
 </template>
 
 <script>
@@ -12,8 +41,39 @@ export default {
 
   computed: {},
 
-  methods: {},
+  methods: {
+    goTo(path) {
+      this.$router.replace(path);
+    },
+  },
 };
 </script>
 <style lang='less' scoped>
+.footer_guide {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #f5f5f5;
+  .guide_item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    text-decoration: none;
+    text-align: center;
+    color: #999999;
+    span {
+      display: block;
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+  .on {
+    color: #6fa6ee;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import '../static/css/reset.css'
 new Vue({
   el: '#app',
   render: h => h(App),
