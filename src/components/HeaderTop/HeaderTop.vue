666fbe1e3750f1b0cd9aa7d31c63b01b8ad0bda9
陈斯婧 <839294976@qq.com>
2022-05-09 10:54:01 +0800

    头部组件

---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <slot name="left"></slot>
+    <div class="header_title">
+      <h2>{{ address }}</h2>
+    </div>
+    <slot name="right"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    address: String,
+  },
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {},
+};
+</script>
+<style lang="less" scoped></style>
